b88e8f49507c38a8775109e7480838c6db067c5a
Raul Souza Lima <raulsouzalima@gmail.com>
2015-10-03 22:27:28 -0300

    refactor on pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ActionController::Base
   layout "application"
 
   def show
-    if %w(informacoes lista-de-presentes fotos).include? params[:page]
+    if params[:page] && template_exists?(params[:page], ["pages"])
       render params[:page]
     else
       render file: "/public/404.html", status: 404
