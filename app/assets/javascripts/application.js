c2a263c6b82b86742b0e1e3e267b8e531fc017ae
Bruno Chagas <brunochagasweb@gmail.com>
2015-10-03 21:28:20 -0300

    Moved js to file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,28 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(function(){
+  var $inputs = "input[type=text], input[type=email]";
+
+  $(document).on("blur", $inputs, function() {
+    var $input = $(this);
+    $input.parent("li").removeClass("focus filled");
+
+    if(!_.isEmpty($input.val())) {
+      $input.parent("li").addClass("filled");
+    }
+  });
+
+  _.each($($inputs), function(el, i){
+    var field = $(el);
+    if(!_.isEmpty(field.val())) {
+      field.parent("li").addClass("filled");
+    }
+  });
+
+  $(document).on("focus", $inputs, function() {
+    var $selector = $(this).parent("li");
+    $selector.addClass("focus");
+  });
+});
